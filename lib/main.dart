6aadce70f6da8ffae716c684f79ee80afb263e76
nck.974 <nck.974@gmail.com>
2023-03-24 23:19:37 +0100

    [FEATURE] Persist settings in the device memory

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,11 @@ import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
+  /// Ensure that the widgets are initialized before using any services or
+  /// features. Otherwise the service used to load the theme settings will trigger
+  /// an exception FlutterError.
+  WidgetsFlutterBinding.ensureInitialized();
+
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
   final settingsController = SettingsController(SettingsService());
--- lib/src/settings/settings_service.dart
+++ lib/src/settings/settings_service.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 /// A service that stores and retrieves user settings.
 ///
@@ -6,12 +8,33 @@ import 'package:flutter/material.dart';
 /// persist the user settings locally, use the shared_preferences package. If
 /// you'd like to store settings on a web server, use the http package.
 class SettingsService {
+  static const _themeModeProperty = 'settings.theme.themeMode';
+  final _logger = Logger();
+
   /// Loads the User's preferred ThemeMode from local or remote storage.
-  Future<ThemeMode> themeMode() async => ThemeMode.system;
+  Future<ThemeMode> themeMode() async {
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+    final String? themeMode = preferences.getString(_themeModeProperty);
+    if (themeMode != null) {
+      if (themeMode == ThemeMode.dark.toString()) {
+        _logger.d("Dark theme mode found the settings");
+        return ThemeMode.dark;
+      } else if (themeMode == ThemeMode.light.toString()) {
+        _logger.d("Light theme mode found the settings");
+        return ThemeMode.light;
+      } else {
+        _logger.d("Invalid theme found in the settings $themeMode");
+      }
+    }
+    return ThemeMode.system;
+  }
 
   /// Persists the user's preferred ThemeMode to local or remote storage.
   Future<void> updateThemeMode(ThemeMode theme) async {
     // Use the shared_preferences package to persist settings locally or the
     // http package to persist settings over the network.
+    _logger.d("Saving in '$_themeModeProperty' value: ${theme.toString()}");
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+    await preferences.setString(_themeModeProperty, theme.toString());
   }
 }
